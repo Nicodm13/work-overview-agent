b94d33431e7cff25f8d41e58cab82ecbade65977
Nicodm13 <Nico24dahl@gmail.com>
2026-08-31 12:52:57 +0200

    Initial Spring Boot MCP setup

---
+++ src/test/java/dk/school/workoverviewagent/ApplicationTest.java
@@ -0,0 +1,12 @@
+package dk.school.workoverviewagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
